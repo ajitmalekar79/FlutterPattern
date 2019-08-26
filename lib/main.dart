fda2638b24fe0eadd8a507e55c377aa5850c8de8
ajitmalekar79 <ajitmalekar79@gmail.com>
2019-08-27 05:23:49 +0530

    Add existing file

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,65 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  var line=List();
   @override
   Widget build(BuildContext context) {
+    printStarWid();
     return MaterialApp(
       title: "My First App",
       home: new Scaffold(
           appBar: new AppBar(
             title: new Text("App Bar Title"),
           ),
-          body: new Text(printStar()+"\n\n"+printSquare())
+          body: new Container(
+            child: ListView.builder(itemBuilder: (BuildContext context, int index){
+              if(index==0){
+                return Card(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                    for(var pattern in line)
+                    Text(pattern)
+              ],
+              ),
+              );
+              }else{
+              return Card(
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+              Row(
+              children: <Widget>[
+                for(int i=0;i<5;i++)
+                Text("* ")
+              ]
+              ),
+              for(int i=0;i<3;i++)
+                Row(
+                children: <Widget>[
+                  Text("*          *")
+                ]
+                ),
+              Row(
+              children:<Widget>[
+              for(int i=0;i<5;i++)
+              Text("* ")
+              ]
+              )
+              ],
+              ),
+              );
+              }
+            },
+            itemCount: 2,),
+          ),
+
       ),
     );
   }
 
-  printStar(){                    //function to print stars
+  printStarWid(){                    //function to print stars
     String temp = "";
     for(int i=0; i<4; i++){
 
@@ -24,32 +69,9 @@ class MyApp extends StatelessWidget {
       for(int j=0; j<=i; j++)
         t=t+"* ";
 
-      temp = temp+t+"\n";
+      line.add(t);
     }
 
-    return temp;
-  }
-
-  //function to print square of stars
-  printSquare(){
-
-    String temp = "";
-    for(int i=0;i<5;i++){
-
-      String t="";
-      for(int j = 0;j<5;j++){
-        if(i==0 || i==4)
-          t=t+"* ";
-        else {
-          if (j == 0 || j == 4)
-            t = t + "*";
-          else
-            t = t + "   ";
-        }
-      }
-
-      temp = temp+t+"\n";
-    }
     return temp;
   }
 }
